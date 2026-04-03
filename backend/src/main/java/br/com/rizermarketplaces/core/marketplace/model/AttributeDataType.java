531f74a91dae7c886a2e4f71abd78448c4c32645
Cristiano Mozena <past.kun@gmail.com>
2026-04-03 16:58:10 -0300

    Remove deprecated marketplace models and migration scripts

--- backend/src/main/java/br/com/rizermarketplaces/core/marketplace/model/AttributeDataType.java
+++
@@ -1,9 +0,0 @@
-package br.com.rizermarketplaces.core.marketplace.model;
-
-public enum AttributeDataType {
-    STRING,
-    NUMBER,
-    BOOLEAN,
-    DATE,
-    JSON
-}
--- backend/src/main/java/br/com/rizermarketplaces/core/marketplace/repository/ProductAttributeValueRepository.java
+++
@@ -1,7 +0,0 @@
-package br.com.rizermarketplaces.core.marketplace.repository;
-
-import br.com.rizermarketplaces.core.marketplace.model.ProductAttributeValue;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductAttributeValueRepository extends JpaRepository<ProductAttributeValue, Long> {
-}
